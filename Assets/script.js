c81351381e3700e514485222f670b83fb8774ea9
jonathanlunabiom <jonny-luna@hotmail.com>
2023-07-18 16:49:58 -0600

    added cities

--- Assets/script.js
+++ Assets/script.js
@@ -2,15 +2,6 @@
 
 $(document).ready(function() {
 
-    fetch('api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=1cbb0bb3c0dae31a0af54f06954be8f4')
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-    })
-
-
     var btnsearch = $('#searchbtn');
     var recentsearch = $('.container-aside');
     var searcharea = $('.textarea');
@@ -18,6 +9,26 @@ $(document).ready(function() {
     var daystoClone = $('.daytoforecast');
     var citynumber = 0;
     var flag = 0;
+
+    function setDataLocation(data){
+        var lat = data[0].lat
+        var lon = data[0].lon
+        var Url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon='+ lon +'&appid=1cbb0bb3c0dae31a0af54f06954be8f4&units=metric';
+        fetch(Url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+            $('.card-body').each(function(i){
+                $(this).children().eq(1).text("Temperature: " + data.list[i].main.temp + " °c");
+                $(this).children().eq(2).text("Wind speed: " + data.list[i].wind.speed + " km/h");
+                $(this).children().eq(3).text("Humidity: " + data.list[i].main.humidity + "%");
+            })
+
+        })
+
+    }
     
     btnsearch.on('click', function(e){
 
@@ -32,6 +43,7 @@ $(document).ready(function() {
         var newElement =  $('<button>');
         var trimtext = searcharea.val().trim()
         var adjustText = trimtext.charAt(0).toUpperCase() + trimtext.slice(1).toLowerCase();
+        sendCurrentCity(adjustText);
         newElement.addClass('bg-secondary rounded text-center text-white mb-2 col-12 border-0 recentcitysearch').text(adjustText);
         recentsearch.prepend(newElement)
         $('#currentcity').text(adjustText + ' now');
@@ -48,7 +60,22 @@ $(document).ready(function() {
         }
         flag++;
     })
-    
+
+    function sendCurrentCity(currentcity){
+        var Url = 'http://api.openweathermap.org/geo/1.0/direct?q='+ currentcity + ',&limit=5&appid=1cbb0bb3c0dae31a0af54f06954be8f4';
+        fetch(Url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            if (data.length !== 0){
+                setDataLocation(data);
+            }else{
+                alert("Error: no city found")
+            }
+        })
+    }
+
     function cloneDays(){
         for(var i=2; i<6;i++){
             var clonedDay = daystoClone.clone();
@@ -58,10 +85,14 @@ $(document).ready(function() {
     }
 
     cloneDays();
-    
+    // console.log($('.card-body'))
 
-    // parent.on (delegate target)
-    // recentsearch.on('click', '.recentcitysearch', function(){
-
+    // $('.card-body').each(function setAtributeDays(data){
+    //     var i = 0;
+    //     $(this).children().eq(1).text("Temperature: " + data.list[0].main.temp + " °c");
+    //     $(this).children().eq(2).text("Wind speed: " + data.list[0].wind.speed + " km/h");
+    //     $(this).children().eq(3).text("Humidity: " + data.list[0].main.humidity + "%");
+    //     i++
     // })
+
 });
