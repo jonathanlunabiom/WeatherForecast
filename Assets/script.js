28467ff463d4916335a02fb731c97a905fa468be
jonathanlunabiom <jonny-luna@hotmail.com>
2023-07-19 11:59:22 -0600

    ready

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    //set variables
     var btnsearch = $('#searchbtn');
     var searcharea = $('.textarea');
     var dayscontainer = $('.dayscontainer');
@@ -7,8 +7,7 @@ $(document).ready(function() {
     var parentCities = $('#parentcities')
     var citynumber = 0;
     var flag = 0;
-    var clearCities = false;
-
+    //fetching the location with the lat and lon obtained from the first fetch.
     function setDataLocation(data){
         var lat = data[0].lat
         var lon = data[0].lon
@@ -18,6 +17,7 @@ $(document).ready(function() {
             return response.json();
         })
         .then(function(data){
+            //giving the humidity, wind and temperature to each card body (6 in total)
             $('.card-body').each(function(i){
                 $(this).children().eq(1).text("Temperature: " + data.list[i].main.temp + "°c");
                 $(this).children().eq(2).text("Wind speed: " + data.list[i].wind.speed + "km/h");
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
                 var icondata = $('.iconweather');
                 var weather = data.list[i].weather[0].main;
-
+                //selecting the icon depending on the weather condition
                 switch (weather){
                     case 'Clouds':
                         icondata[i].src = './Assets/img/clouds.png'
@@ -48,19 +48,19 @@ $(document).ready(function() {
             })
         })
     }
-    
+    //function to create an object of current searches
     function createbutton(adjustText){
         var newElement =  $('<button>');
         newElement.addClass('bg-secondary rounded text-center text-white mb-2 col-12 border-0 recentcitysearch').text(adjustText);
         sendCurrentCity(adjustText);
         parentCities.prepend(newElement)
     }
-    
+    //getting the textarea input and tranforming it.
     btnsearch.on('click', function(e){
         e.preventDefault();
-
+        //when starting the code the main container will show
         $('.startsearch').removeClass('hide')
-
+        //if theres no data it will return you
         if(searcharea.val() == ''){
             alert('no data input');
             return;
@@ -69,20 +69,20 @@ $(document).ready(function() {
         var trimtext = searcharea.val().trim()
         var adjustText = trimtext.charAt(0).toUpperCase() + trimtext.slice(1).toLowerCase();
         createbutton(adjustText);
-
+        //storing 4 elements on the localStorage
         if(citynumber < 4){
             localStorage.setItem(citynumber,adjustText)
             citynumber++;
         }else{
             citynumber = 0; 
         }
-
+        //removing button when passing the condition
         if(flag > 2){
             parentCities.children().last().remove();
         }
         flag++;
     })
-
+    //the first fetching where it determines the lat and lon and it sends it to the fetch above
     function sendCurrentCity(currentcity){
         var Url = 'https://api.openweathermap.org/geo/1.0/direct?q='+ currentcity + ',&limit=5&appid=1cbb0bb3c0dae31a0af54f06954be8f4';
         fetch(Url)
@@ -96,6 +96,7 @@ $(document).ready(function() {
             }else{
                 $('#currentcity').text('City not found');
                 $('.card-body').each(function(){
+                    //when theres no data it will clear the elements
                     $(this).children().eq(1).text('')
                     $(this).children().eq(2).text('')
                     $(this).children().eq(3).text('')
@@ -103,7 +104,7 @@ $(document).ready(function() {
             }
         })
     }
-
+    //cloning the first day x4 times
     function cloneDays(){
         for(var i=2; i<6;i++){
             var clonedDay = daystoClone.clone();
@@ -113,17 +114,18 @@ $(document).ready(function() {
     }
 
     cloneDays();
-
+    //when you click on the button of the local storage it will send the value to the first fetch
+    //e target of delegate
     function currentCityTo(e){
         $('.startsearch').removeClass('hide')
         var btnclick = $(e.target);
         var  value = btnclick.html();
         sendCurrentCity(value);
     }
-    
+    //delegating to children
     parentCities.on('click','.recentcitysearch', currentCityTo)
 
-
+    //getting the local storage info and creating a button for each
     function setLocalStorageInfo(){
         for(var i = 0; i < localStorage.length;i++){
             var setItem = localStorage.getItem(i)
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# WeatherForecast
+# WorkSchedule - jQuery
+On this challenge im working from an empty file creating each element, the html, the css and the js. Using the 5-day weather API.
+
+## Functionality
+
+When enter the page for the first time you'll notice there's no current searches. In order to store your data you must search for a city. If the city doesnt exist it will show you on the main container "city not found", and if you didnt put any text value it may throw you an alert.
+When searching for a real city it will display the current day (blue container) and the next 5 days (black container), with information about humidity, temperature and wind speed, you can search as many cities as you want but the localstorage will only retrieve the last 4. You may be seeing your last data even when reload the page. 
+
+
+## User Story
+
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Acceptance Criteria
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+##Page SS
+
+<img width="518" alt="WeatherF.png" src="WeatherF.png">
+
+##Deployed Webpage
+https://jonathanlunabiom.github.io/WeatherForecast/
